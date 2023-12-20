8146ead5c4dad20be0248a1c6aba7d9fd93461a0
David Matheson <davidfmatheson@gmail.com>
2023-12-19 19:15:41 -0500

    Move nvim config to LazyVim directory to try out LazyVim.

---
+++ nvim-lazyvim/.config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,22 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "bash",
+        "groovy",
+        "html",
+        "javascript",
+        "json",
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "python",
+        "regex",
+        "typescript",
+        "vim",
+        "yaml",
+      },
+    },
+  },
+}
