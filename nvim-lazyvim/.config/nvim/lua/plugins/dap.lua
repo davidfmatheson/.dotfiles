9887b09f14978c7f786edb4cfe6942f9d9a74105
David Matheson <davidfmatheson@gmail.com>
2023-12-29 23:14:09 -0500

    Still trying to get pwa-chrome to work.

--- nvim-lazyvim/.config/nvim/lua/plugins/dap.lua
+++ nvim-lazyvim/.config/nvim/lua/plugins/dap.lua
@@ -1,36 +1,67 @@
 return {
   {
     "mfussenegger/nvim-dap",
+    -- dependencies = {
+    --   {
+    --     "mxsdev/nvim-dap-vscode-js",
+    --     config = function()
+    --       require("dap-vscode-js").setup({
+    --         node_path = "node",
+    --         debugger_cmd = {
+    --           require("mason-registry").get_package("js-debug-adapter"):get_install_path()
+    --             .. "/js-debug-adapter",
+    --         },
+    --         adapters = {
+    --           "pwa-node",
+    --           "pwa-chrome",
+    --         },
+    --         log_file_path = vim.fn.stdpath("log") .. "/dap_vscode_js.log",
+    --         log_file_level = vim.log.levels.DEBUG,
+    --         log_console_level = vim.log.levels.ERROR,
+    --       })
+    --     end,
+    --   },
+    -- },
     opts = function()
       local dap = require("dap")
       local file = vim.fn.expand("%:p")
-      local projectFolder = ""
+      local projectfolder = ""
       local cwd = vim.fn.getcwd()
       if string.find(file, cwd .. "/apps/") or string.find(file, cwd .. "/libs/") then
-        projectFolder = string.match(file, cwd .. "(.-/[^/]+)/src")
+        projectfolder = string.match(file, cwd .. "(.-/[^/]+)/src")
       end
       for _, language in ipairs({ "typescript", "javascript", "typescriptreact", "javascriptreact" }) do
         dap.configurations[language] = {
           {
             type = "pwa-node",
             request = "launch",
-            name = "Launch file",
+            name = "launch file",
             program = "${file}",
-            cwd = "${workspaceFolder}",
+            cwd = "${workspacefolder}",
           },
           {
             type = "pwa-node",
             request = "attach",
-            name = "Attach by process",
-            processId = require("dap.utils").pick_process,
-            cwd = "${workspaceFolder}" .. projectFolder,
+            name = "attach by process",
+            processid = require("dap.utils").pick_process,
+            cwd = "${workspacefolder}" .. projectfolder,
           },
           {
             type = "pwa-node",
             request = "attach",
-            name = "Attach by port",
+            name = "attach by port",
             port = 9229,
-            cwd = "${workspaceFolder}" .. projectFolder,
+            cwd = "${workspacefolder}" .. projectfolder,
+          },
+          {
+            type = "pwa-chrome",
+            request = "launch",
+            name = "launch & debug chrome",
+            url = "http://localhost:4200",
+            webroot = vim.fn.getcwd(),
+            protocol = "inspector",
+            sourcemaps = true,
+            userdatadir = false,
           },
         }
       end
