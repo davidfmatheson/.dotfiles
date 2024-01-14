5ec986f89d17af023e914e9d87e95573d3bbe282
David Matheson <davidfmatheson@gmail.com>
2024-01-14 15:38:41 -0500

    Nx changes for lazy.

--- nvim-lazyvim/.config/nvim/lua/plugins/nx.lua
+++ nvim-lazyvim/.config/nvim/lua/plugins/nx.lua
@@ -2,19 +2,16 @@ return {
   {
     "Equilibris/nx.nvim",
 
-    lazy = false,
-
     dependencies = {
       "nvim-telescope/telescope.nvim",
     },
 
-    config = function()
-      require("nx").setup({
-        nx_cmd_root = "npx nx",
-        command_runner = require("nx.command-runners").terminal_cmd(),
-        form_renderer = require("nx.form-renderers").telescope(),
-        read_init = true,
-      })
-    end,
+    opts  = {
+      nx_cmd_root = "npx nx",
+    },
+
+    keys = {
+      { "<leader>nx", "<cmd>Telescope nx actions<CR>", desc = "nx actions"}
+    },
   },
 }
