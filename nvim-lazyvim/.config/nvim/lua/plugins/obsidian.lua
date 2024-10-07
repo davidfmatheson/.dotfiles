5b338262f9ed4580342e5377d6fbc5efd60c816d
David Matheson <davidfmatheson@gmail.com>
2024-10-06 21:04:10 -0400

    Update Obsidian vault path

--- nvim-lazyvim/.config/nvim/lua/plugins/obsidian.lua
+++ nvim-lazyvim/.config/nvim/lua/plugins/obsidian.lua
@@ -3,8 +3,8 @@ return {
   version = "*",  -- recommended, use latest release instead of latest commit
   lazy = true,
   event = {
-    "BufReadPre " .. vim.fn.expand "~" .. "/Projects/vault/**.md",
-    "BufNewFile " .. vim.fn.expand "~" .. "/Projects/vault/**.md",
+    "BufReadPre " .. vim.fn.expand "~" .. "/Library/Mobile Documents/iCloud~md~obsidian/Documents/Vault/**.md",
+    "BufNewFile " .. vim.fn.expand "~" .. "/Library/Mobile Documents/iCloud~md~obsidian/Documents/Vault/**.md",
   },
   dependencies = {
     "nvim-lua/plenary.nvim",
@@ -13,7 +13,7 @@ return {
     workspaces = {
       {
         name = "personal",
-        path = "~/Projects/vault",
+        path = "~/Library/Mobile Documents/iCloud~md~obsidian/Documents/Vault",
       },
     },
   },
