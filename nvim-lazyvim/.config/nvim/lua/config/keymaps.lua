664f58876fe395a11b5985964f5b73d84d0a7110
David Matheson <davidfmatheson@gmail.com>
2025-07-12 22:21:32 -0400

    Fixing keymaps

--- nvim-lazyvim/.config/nvim/lua/config/keymaps.lua
+++ nvim-lazyvim/.config/nvim/lua/config/keymaps.lua
@@ -2,9 +2,9 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 -- Move Lines, but use Shift so as not to interfere with Kitty
-vim.keymap.set("n", "<D-S-j>", "<cmd>m .+1<cr>==", { desc = "Move Down" })
-vim.keymap.set("n", "<D-S-k>", "<cmd>m .-2<cr>==", { desc = "Move Up" })
-vim.keymap.set("i", "<D-S-j>", "<esc><cmd>m .+1<cr>==gi", { desc = "Move Down" })
-vim.keymap.set("i", "<D-S-k>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move Up" })
-vim.keymap.set("v", "<D-S-j>", ":m '>+1<cr>gv=gv", { desc = "Move Down" })
-vim.keymap.set("v", "<D-S-k>", ":m '<-2<cr>gv=gv", { desc = "Move Up" })
+vim.keymap.set("n", "<D-j>", "<cmd>m .+1<cr>==", { desc = "Move Down" })
+vim.keymap.set("n", "<D-k>", "<cmd>m .-2<cr>==", { desc = "Move Up" })
+vim.keymap.set("i", "<D-j>", "<esc><cmd>m .+1<cr>==gi", { desc = "Move Down" })
+vim.keymap.set("i", "<D-k>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move Up" })
+vim.keymap.set("v", "<D-j>", ":m '>+1<cr>gv=gv", { desc = "Move Down" })
+vim.keymap.set("v", "<D-k>", ":m '<-2<cr>gv=gv", { desc = "Move Up" })
